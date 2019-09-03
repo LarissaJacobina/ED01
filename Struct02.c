934e7894a5437cb0b3efbf9e23b636c374e238ce
LarissaJacobina <45287757+LarissaJacobina@users.noreply.github.com>
2019-09-03 09:53:16 -0300

    Add files via upload

---
+++ Struct02.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <string.h>
+#define TAM 30
+
+
+typedef struct {
+    char nome[TAM];
+     char cpf[TAM];
+    float peso;
+    int idade;
+    float altura;
+} Pessoa;
+
+void imprimirPessoa(Pessoa p) {
+    printf(" Nome: %s\n CPF: %s\n Idade: %d\n Peso: %.2f\n Altura: %.2f\n", p.nome, p.cpf, p.idade, p.peso, p.altura);
+}
+
+void setPessoa(Pessoa *p, char nome[], char cpf[], int idade, float peso, float altura) {
+
+    for (int i = 0; i < TAM; i++) {
+        p->nome[i] = nome[i];
+    }
+    for (int i = 0; i < TAM; i++) {
+        p->cpf[i] = cpf[i];
+    }
+    (*p).idade = idade;
+    p->peso = peso;
+    p->altura = altura;
+}
+
+int main() {
+    Pessoa pessoa01;
+    char nome[TAM] = "Larissa Jacobina";
+    char cpf[TAM] = "111.111.111-00";
+    setPessoa(&pessoa01, nome, cpf, 20, 72, 1.70);
+    imprimirPessoa(pessoa01);
+    return 0;
+}
